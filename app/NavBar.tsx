309305ef2557eb2085215aefd345c6743a58d9db
Ryan Biondo <biondoryan.dev@gmail.com>
2023-11-09 20:50:31 -0700

    Build the NavBar

---
+++ app/NavBar.tsx
@@ -0,0 +1,29 @@
+import Link from 'next/link';
+import React from 'react';
+import { AiFillBug } from 'react-icons/ai';
+
+const NavBar = () => {
+  const links = [
+    { href: '/', label: 'Dashboard' },
+    { href: '/issues', label: 'Issues' },
+  ];
+
+  return (
+    <nav className="flex space-x-6 border-b mb-5 px-5 h-14 items-center">
+      <Link href="/">
+        <AiFillBug />
+      </Link>
+      <ul className="flex space-x-6">
+        {links.map(({ href, label }) => (
+          <Link
+            href={href}
+            className="text-zinc-500 hover:text-zinc-800 transition-colors">
+            {label}
+          </Link>
+        ))}
+      </ul>
+    </nav>
+  );
+};
+
+export default NavBar;
