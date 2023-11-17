63c68d0364f6b394055edbd929e8114af279a507
Ryan Biondo <biondoryan.dev@gmail.com>
2023-11-17 15:14:57 -0700

    Add additional loading skeletons

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,10 +1,9 @@
 import prisma from '@/prisma/client';
 import { Table } from '@radix-ui/themes';
-import React from 'react';
-import IssueStatusBadge from '../components/IssueStatusBadge';
 import delay from 'delay';
-import IssueActions from './IssueActions';
+import IssueStatusBadge from '../components/IssueStatusBadge';
 import Link from '../components/Link';
+import IssueActions from './IssueActions';
 
 const IssuesPage = async () => {
   const issues = await prisma.issue.findMany();
