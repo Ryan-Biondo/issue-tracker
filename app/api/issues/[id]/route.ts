d2c629615a08e40b22f9c1e241a9c3c40f59c18a
Ryan Biondo <biondoryan.dev@gmail.com>
2023-11-22 13:33:52 -0700

    Secure the Application

--- app/api/issues/[id]/route.ts
+++ app/api/issues/[id]/route.ts
@@ -1,5 +1,7 @@
+import authOptions from "@/app/auth/authOptions";
 import { issueSchema } from "@/app/validationSchema";
 import prisma from "@/prisma/client";
+import { getServerSession } from "next-auth";
 import { NextRequest, NextResponse } from "next/server";
 
 interface Props {
@@ -7,6 +9,10 @@ params: {id: string}
 }
 
 export async function PATCH(request: NextRequest, {params}: {params: {id: string}}) {
+    const session = await getServerSession(authOptions);
+    if (!session) {
+        return NextResponse.json({error: 'Unauthorized'}, {status: 401})
+    }
     const body = await request.json();
     const validation = issueSchema.safeParse(body);
     if(!validation.success) {
@@ -25,6 +31,10 @@ export async function PATCH(request: NextRequest, {params}: {params: {id: string
 }
 
 export async function DELETE(request: NextRequest, {params}: {params: {id: string}}) {
+    const session = await getServerSession(authOptions);
+    if (!session) {
+        return NextResponse.json({error: 'Unauthorized'}, {status: 401})
+    }
     const issue = await prisma.issue.findUnique(
         {where: {id: parseInt(params.id)}}
     )
