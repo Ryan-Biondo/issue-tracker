9c5cfdb1b034a062ae01e98f95f088210a92f61c
Ryan Biondo <biondoryan.dev@gmail.com>
2023-12-06 13:32:26 -0700

    Comment migration

--- prisma/migrations/20231205205816_initial_migration/migration.sql
+++ prisma/migrations/20231205205816_initial_migration/migration.sql
@@ -53,13 +53,3 @@ CREATE TABLE `VerificationToken` (
     UNIQUE INDEX `VerificationToken_token_key`(`token`),
     UNIQUE INDEX `VerificationToken_identifier_token_key`(`identifier`, `token`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
--- CreateTable
-CREATE TABLE `Comment` (
-    `id` INTEGER NOT NULL AUTO_INCREMENT,
-    `content` TEXT NOT NULL,
-    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
-    `updatedAt` DATETIME(3) NOT NULL,
-    `issueId` INTEGER NOT NULL,
-    `authorEmail` VARCHAR(191) NOT NULL,
-    PRIMARY KEY (`id`)
-) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
